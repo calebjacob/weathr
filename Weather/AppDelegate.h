62badef985aa65cda49fb4d136d4980113de4be4
iamceege <hello@calebjacob.com>
2014-05-20 10:33:14 -0600

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Caleb Jacob on 5/20/14.
+//  Copyright (c) 2014 Caleb Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Weather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by Caleb Jacob on 5/20/14.
+//  Copyright (c) 2014 Caleb Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
