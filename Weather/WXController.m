19339e854225a4a12fc08f8acd2e7e133d691efa
iamceege <hello@calebjacob.com>
2014-05-23 15:30:50 -0600

    New styling for the first display

--- Weather/WXController.m
+++ Weather/WXController.m
@@ -15,6 +15,7 @@
 @property (nonatomic, strong) UIImageView *backgroundImageView;
 @property (nonatomic, strong) UIImageView *blurredImageView;
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIView *circleView;
 @property (nonatomic, assign) CGFloat screenHeight;
 @property (nonatomic, strong) NSDateFormatter *hourlyFormatter;
 @property (nonatomic, strong) NSDateFormatter *dailyFormatter;
@@ -72,46 +73,67 @@
     
     CGRect headerFrame = [UIScreen mainScreen].bounds;
     CGFloat inset = 20;
-    CGFloat temperatureHeight = 110;
-    CGFloat hiloHeight = 40;
-    CGFloat iconHeight = 30;
+    CGFloat circleSize = 200;
+    CGFloat temperatureHeight = 60;
+    CGFloat hiHeight = 30;
+    CGFloat loHeight = 30;
+    CGFloat conditionHeight = 30;
     
-    CGRect hiloFrame = CGRectMake(inset,
-                                  headerFrame.size.height - hiloHeight,
-                                  headerFrame.size.width - (inset * 2),
-                                  hiloHeight);
+    CGRect circleFrame = CGRectMake((headerFrame.size.width - circleSize) / 2,
+                                    (headerFrame.size.height - circleSize) / 2,
+                                    circleSize,
+                                    circleSize);
     
-    CGRect temperatureFrame = CGRectMake(inset,
-                                         headerFrame.size.height - (temperatureHeight + hiloHeight),
-                                         headerFrame.size.width - (inset * 2),
+    CGRect temperatureFrame = CGRectMake(circleFrame.origin.x,
+                                         circleFrame.origin.y + ((circleFrame.size.height - temperatureHeight) / 2),
+                                         circleSize,
                                          temperatureHeight);
     
-    CGRect iconFrame = CGRectMake(inset,
-                                  temperatureFrame.origin.y - iconHeight,
-                                  iconHeight,
-                                  iconHeight);
+    CGRect conditionsFrame = CGRectMake(circleFrame.origin.x,
+                                  temperatureFrame.origin.y - conditionHeight - 15,
+                                  circleSize,
+                                  conditionHeight);
     
-    CGRect conditionsFrame = iconFrame;
-    conditionsFrame.size.width = self.view.bounds.size.width - (((inset * 2) + iconHeight) + 10);
-    conditionsFrame.origin.x = iconFrame.origin.x + (iconHeight + 10);
+    CGRect hiFrame = CGRectMake(circleFrame.origin.x,
+                                  temperatureFrame.origin.y + temperatureFrame.size.height + 15,
+                                  (circleSize / 2) - 5,
+                                  hiHeight);
+    
+    CGRect loFrame = hiFrame;
+    loFrame.origin.x = circleFrame.origin.x + (circleFrame.size.width / 2);
     
     UIView *header = [[UIView alloc] initWithFrame:headerFrame];
     header.backgroundColor = [UIColor clearColor];
     self.tableView.tableHeaderView = header;
     
+    self.circleView = [[UIView alloc] initWithFrame:circleFrame];
+    self.circleView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.6];
+    self.circleView.layer.cornerRadius = circleSize / 2;
+    [header addSubview:self.circleView];
+    
     UILabel *temperatureLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
     temperatureLabel.backgroundColor = [UIColor clearColor];
     temperatureLabel.textColor = [UIColor whiteColor];
     temperatureLabel.text = @"0°";
-    temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:120];
+    temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:76];
+    temperatureLabel.textAlignment = NSTextAlignmentCenter;
     [header addSubview:temperatureLabel];
     
-    UILabel *hiloLabel = [[UILabel alloc] initWithFrame:hiloFrame];
-    hiloLabel.backgroundColor = [UIColor clearColor];
-    hiloLabel.textColor = [UIColor whiteColor];
-    hiloLabel.text = @"0° / 0°";
-    hiloLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:28];
-    [header addSubview:hiloLabel];
+    UILabel *hiLabel = [[UILabel alloc] initWithFrame:hiFrame];
+    hiLabel.backgroundColor = [UIColor clearColor];
+    hiLabel.textColor = [UIColor colorWithRed:1 green:0.384 blue:0.357 alpha:1];
+    hiLabel.text = @"0°";
+    hiLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+    hiLabel.textAlignment = NSTextAlignmentRight;
+    [header addSubview:hiLabel];
+    
+    UILabel *loLabel = [[UILabel alloc] initWithFrame:loFrame];
+    loLabel.backgroundColor = [UIColor clearColor];
+    loLabel.textColor = [UIColor colorWithRed:0.051 green:0.38 blue:0.682 alpha:1];
+    loLabel.text = @"0°";
+    loLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+    loLabel.textAlignment = NSTextAlignmentLeft;
+    [header addSubview:loLabel];
     
     UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, inset, headerFrame.size.width, 30)];
     cityLabel.backgroundColor = [UIColor clearColor];
@@ -124,15 +146,10 @@
     UILabel *conditionsLabel = [[UILabel alloc] initWithFrame:conditionsFrame];
     conditionsLabel.backgroundColor = [UIColor clearColor];
     conditionsLabel.textColor = [UIColor whiteColor];
-    conditionsLabel.text = @"Clear";
+    conditionsLabel.text = @"";
     conditionsLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+    conditionsLabel.textAlignment = NSTextAlignmentCenter;
     [header addSubview:conditionsLabel];
-    
-    UIImageView *iconView = [[UIImageView alloc] initWithFrame:iconFrame];
-    iconView.contentMode = UIViewContentModeScaleAspectFill;
-    iconView.backgroundColor = [UIColor clearColor];
-    iconView.image = [UIImage imageNamed:@"weather-clear"];
-    [header addSubview:iconView];
     
     [[WXManager sharedManager] findCurrentLocation];
     
@@ -141,7 +158,6 @@
             temperatureLabel.text = [NSString stringWithFormat:@"%.0f°", newCondition.temperature.floatValue];
             conditionsLabel.text = [newCondition.condition capitalizedString];
             cityLabel.text = [newCondition.locationName capitalizedString];
-            iconView.image = [UIImage imageNamed:[newCondition imageName]];
         }
     }];
     
@@ -164,12 +180,15 @@
         }
     }];
     
-    RAC(hiloLabel, text) = [[RACSignal combineLatest:@[
-                                                      RACObserve([WXManager sharedManager], currentCondition.tempHigh),
-                                                      RACObserve([WXManager sharedManager], currentCondition.tempLow)]
-                                             reduce:^(NSNumber *high, NSNumber *low) {
-                                                 return [NSString stringWithFormat:@"%.0f° / %.0f°", high.floatValue, low.floatValue];
+    RAC(hiLabel, text) = [[RACSignal combineLatest:@[RACObserve([WXManager sharedManager], currentCondition.tempHigh)]
+                                             reduce:^(NSNumber *high) {
+                                                 return [NSString stringWithFormat:@"%.0f°", high.floatValue];
                                              }] deliverOn:RACScheduler.mainThreadScheduler];
+    
+    RAC(loLabel, text) = [[RACSignal combineLatest:@[RACObserve([WXManager sharedManager], currentCondition.tempLow)]
+                                            reduce:^(NSNumber *low) {
+                                                return [NSString stringWithFormat:@"%.0f°", low.floatValue];
+                                            }] deliverOn:RACScheduler.mainThreadScheduler];
 }
 
 - (void)viewWillLayoutSubviews {
